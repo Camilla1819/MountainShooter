4f56dac8fe540681975a407c0d3b1ef22c340a1f
camillavz <camillaoliveira831@gmail.com>
2026-07-15 22:40:27 -0300

    Creating Mediator Pattern. Enemies Destruction.

--- code/Const.py
+++ code/Const.py
@@ -17,8 +17,22 @@ ENTITY_SPEED = {
     'Level1Bg6': 6,
     'Player1': 3,
     'Player2': 3,
-    'Enemy1':2,
-    'Enemy2':1,
+    'Enemy1': 2,
+    'Enemy2': 1,
+}
+
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
 }
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -38,7 +52,7 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
-#s
+# s
 SPAWN_TIMET = 4000
 
 # W
