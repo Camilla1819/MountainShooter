3fcd635a3f31ea42c4d84c38b1e5b03e84ba0095
camillavz <camillaoliveira831@gmail.com>
2026-07-22 18:17:18 -0300

    Level 2 implemention

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -19,11 +19,19 @@ class EntityFactory:
                     list_Bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_Bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_Bg
+
+            case 'Level2Bg':
+                list_Bg = []
+                for i in range(5):
+                    list_Bg.append(Background(f'Level2Bg{i}', (0, 0)))
+                    list_Bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
+                return list_Bg
+
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
                 return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
             case 'Enemy1':
-                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT -40)))
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
-                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT -40)))
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
